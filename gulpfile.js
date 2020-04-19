4153640d7af44e086e3fe37e18321f019174233b
MD. SOJIB MIYA <lisancseru@gmail.com>
2020-04-19 21:16:56 +0600

    demo pdf file updated

--- gulpfile.js
+++ gulpfile.js
@@ -765,9 +765,7 @@ function buildGeneric(defines, dir) {
       )
       .pipe(gulp.dest(dir + "web")),
 
-    gulp
-      .src("web/compressed.tracemonkey-pldi-09.pdf")
-      .pipe(gulp.dest(dir + "web")),
+    gulp.src("web/demo.pdf").pipe(gulp.dest(dir + "web")),
   ]);
 }
 
@@ -907,9 +905,7 @@ gulp.task(
         )
         .pipe(gulp.dest(MINIFIED_DIR + "web")),
 
-      gulp
-        .src("web/compressed.tracemonkey-pldi-09.pdf")
-        .pipe(gulp.dest(MINIFIED_DIR + "web")),
+      gulp.src("web/demo.pdf").pipe(gulp.dest(MINIFIED_DIR + "web")),
     ]);
   })
 );
